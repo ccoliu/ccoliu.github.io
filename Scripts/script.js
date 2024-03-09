440fa204d6f287f37688a71c8255f700588375ca
unknown <whps970083@gmail.com>
2024-03-09 21:39:30 +0800

    IP

--- Scripts/script.js
+++ Scripts/script.js
@@ -136,7 +136,7 @@ function enhanceTextInput(event, element) {
 
 // Function to send code data to the server
 function sendDataToAnalyzeServer(code) {
-  fetch("http://127.0.0.1:5000/process_code", {
+  fetch("https://140.118.184.235:5000/process_code", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
