5523958db71cbefa194fce7acedf1540fa661c5e
whps970083 <whps970083@gmail.com>
2024-12-11 22:40:43 +0800

    修復捲動條自動下捲

--- Scripts/similarity.js
+++ Scripts/similarity.js
@@ -239,7 +239,7 @@ submitbtn.addEventListener("click", async () => {
         document.querySelector('.AImsg').style.display = "block";
         document.querySelector('.textOutput').style.display = "block";
         document.querySelector('.textOutput').value = data['result'];
-        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
     })
     .catch((error) => {
         console.error("Error:", error);
